ff9d1bcdcee222a79abc80d39279c76ae8acf834
soal <sorrow.about.alice@gmail.com>
2016-04-17 03:46:57 +0300

    Add date and fact models

---
+++ ortelius/models/Date.py
@@ -0,0 +1,66 @@
+import math
+from ortelius import db
+
+
+class Date(db.Model):
+    """Date model"""
+    __tablename__ = 'date'
+
+    def __init__(self, date=None, year_number=None):
+        self.date = date
+        self.year_number = year_number
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    year_number = db.Column(db.Integer, db.ForeignKey('year.number'))
+
+
+class Year(db.Model):
+    """Year model"""
+    __tablename__ = 'year'
+
+    def __init__(self, number=None, dates=None, century_number=None):
+        self.number = number
+        self.century_number = century_number
+        if dates:
+            self.dates = dates
+
+    number = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    dates = db.relationship('Date', backref=db.backref('year', lazy='select'), lazy='dynamic')
+    # century is declared in Century class via backref
+    century_number = db.Column(db.Integer, db.ForeignKey('century.number'))
+
+    def calculate_century(self):
+        return math.floor(self.number / 100) if (self.number / 100) < 0 else math.floor(self.number / 100) + 1
+
+
+class Century(db.Model):
+    """Century model"""
+    __tablename__ = 'century'
+
+    def __init__(self, number=None, years=None, millenium_number=None):
+        self.number = number
+        self.millenium_number = millenium_number
+        if years:
+            self.years = years
+
+    number = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    years = db.relationship('Year', backref=db.backref('century', lazy='select'), lazy='dynamic')
+    millenium_number = db.Column(db.Integer, db.ForeignKey('millenium.number'))
+    # millenium is declared in Millenium class via backref
+
+    def calculate_millenium(self):
+        return math.floor(self.number / 10) if (self.number / 10) < 0 else math.floor(self.number / 10) + 1
+
+
+class Millenium(db.Model):
+    """Millenium model"""
+    __tablename__ = 'millenium'
+
+    def __init__(self, number=None, centuries=None):
+        self.number = number
+        if centuries:
+            self.centuries = centuries
+
+    number = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    centuries = db.relationship('Century', backref=db.backref('millenium', lazy='select'), lazy='dynamic')
--- ortelius/models/Fact.py
+++ ortelius/models/Fact.py
@@ -1,30 +1,77 @@
 from ortelius import db
+from ortelius.models.Date import Millenium, Century, Year, Date
 
 
 class Fact(db.Model):
     """Fact model"""
-    __tablename__ = 'facts_table'
+    __tablename__ = 'fact'
 
-    # def __init__(self, arg):
-    #     super(Fact, self).__init__()
-    #     self.arg = arg
+    def __init__(self, name=None,
+                 label=None,
+                 description=None,
+                 weight=None,
+                 fact_type=None,
+                 start_date=None,
+                 end_date=None,
+                 text=None,
+                 shape=None):
+        self.name = name
+        self.label = label
+        self.description = description
+        self.weight = weight
+        self.fact_type = fact_type
+        self.text = text
+        self.shape = shape
+        if start_date:
+            self.start_date = start_date[0]
+            self.start_date_millenium = start_date[1]
+            self.start_date_century = start_date[2]
+            self.start_date_year = start_date[3]
+        if end_date:
+            self.end_date = end_date[0]
+            self.end_date_millenium = end_date[1]
+            self.end_date_century = end_date[2]
+            self.end_date_year = end_date[3]
 
-    # id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    label = db.Column(db.Unicode(255))
+    description = db.Column(db.UnicodeText, server_default='No description')
+    weight = db.Column(db.Integer, nullable=False, server_default='5')  # NOTE: May be enum type?
+    fact_type = db.Column(db.Integer, db.ForeignKey('fact_type.id'))
+    start_date_millenium = db.Column(db.Integer, db.ForeignKey('millenium.number'), nullable=True)  # FIXME: What should be here???
+    start_date_century = db.Column(db.Integer, db.ForeignKey('century.number'), nullable=True)  # FIXME: What should be here???
+    start_date_year = db.Column(db.Integer, db.ForeignKey('year.number'), nullable=True)  # FIXME: What should be here???
+    start_date = db.Column(db.Integer, db.ForeignKey('date.id'), nullable=True)  # FIXME: What should be here???
+    end_date_millenium = db.Column(db.Integer, db.ForeignKey('millenium.number'), nullable=True)  # FIXME: What should be here???
+    end_date_century = db.Column(db.Integer, db.ForeignKey('century.number'), nullable=True)  # FIXME: What should be here???
+    end_date_year = db.Column(db.Integer, db.ForeignKey('year.number'), nullable=True)  # FIXME: What should be here???
+    end_date = db.Column(db.Integer, db.ForeignKey('date.id'), nullable=True)  # FIXME: What should be here???
+    text = db.Column(db.UnicodeText, server_default='No text')
 
-    # name = db.Column(db.String(255), nullable=False, unique=True)
-    # label = db.Column(db.Unicode(255), nullable=False)
-    # description = db.Column(db.UnicodeText, nullable=True, server_default='')
-    # text = db.Column(db.UnicodeText, nullable=True, server_default='')
-    # weight = db.Column(db.Integer, nullable=True, server_default=5)
+    # shape = db.Column(db.Integer, db.ForeignKey('shape.id'))
+    # shape = db.Column(db.Integer, db.ForeignKey('shape.id'))
+    # processes is declared in Process model via backref
+    # personas is declared in Persona model via backref
+    # georegions is declared in Georegion model via backref
+    # hist_regions is declared in Hist_region model via backref
 
-    # start_date = db.relationship()
-    # end_date = db.relationship()
+    def __repr__(self):
+        return '<Fact %s, shows as %s>' % (self.name, self.label)
 
-    # shape = db.relationship('Shape',
-    #                         secondary="facts_shape",
-    #                         backref=db.backref('facts', lazy='dynamic'))
-    # processes = db.relationship('Process',
-    #                             secondary="facts_processes",
-    #                             backref=db.backref('facts', lazy='dynamic'))
 
-    # personas = db.relationship()
+class FactType(db.Model):
+    """Fact types model"""
+    __tablename__ = 'fact_type'
+
+    def __init__(self, name=None, lable=None):
+        self.name = name
+        self.label = label
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    label = db.Column(db.Unicode(120), nullable=False, unique=True)
+    type_facts = db.relationship('Fact', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Fact type %r, shows as %r>' % (self.name, self.label)
