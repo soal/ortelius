3e36a93eaff60ee8a5b2b657ba69a7a5de41105c
soal <sorrow.about.alice@gmail.com>
2016-04-18 20:00:41 +0300

    Add SHape model

--- ortelius/models/Shape.py
+++ ortelius/models/Shape.py
@@ -1,10 +1,35 @@
-# from ortelius import db
-#
-#
-# class Shape(db.Model):
-#     """Shape table"""
-#     __tablename__ = 'shape'
-#
-#     def __init__(self, arg):
-#         super(Shape, self).__init__()
-#         self.arg = arg
+from ortelius import db
+from ortelius.models.Date import Date
+
+shapes_coordinates = db.Table('shapes_coordinates',
+    db.Column('shape_id', db.Integer, db.ForeignKey('shape.id')),
+    db.Column('coordinates_id', db.Integer, db.ForeignKey('coordinates.id'))
+)
+
+
+class Shape(db.Model):
+    """Shape model"""
+    __tablename__ = 'shape'
+
+    def __init__(self):
+        pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Integer, db.ForeignKey('date.id'))
+    end_date = db.Column(db.Integer, db.ForeignKey('date.id'))
+    coordinates = db.relationship('Coordinates',
+                                  secondary=shapes_coordinates,
+                                  backref=db.backref('shapes', lazy='dynamic'))
+
+
+class Coordinates(db.Model):
+    """Coordinates model"""
+    __tablename__ = 'coordinates'
+
+    def __init__(self):
+        pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.Float, nullable=False)
+    long = db.Column(db.Float, nullable=False)
+    # shapes is declared in Shape class via backref
