1427b91ec07c5591115f01f85e29a76678f5708d
soal <sorrow.about.alice@gmail.com>
2016-04-19 18:03:57 +0300

    Remove unnessesary Shape.py

--- ortelius/models/Shape.py
+++
@@ -1,7 +0,0 @@
-from ortelius import db
-from ortelius.models.Date import Date
-
-shapes_coordinates = db.Table('shapes_coordinates',
-    db.Column('shape_id', db.Integer, db.ForeignKey('shape.id')),
-    db.Column('coordinates_id', db.Integer, db.ForeignKey('coordinates.id'))
-)
