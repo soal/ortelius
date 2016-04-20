d6b1e10f5a2e9b3b2d4db8d15d325aca8e387a4d
soal <sorrow.about.alice@gmail.com>
2016-04-20 11:59:20 +0300

    Refactor Date model Move from one-to-many to many-to-one relationships Add create classmethod for date

--- ortelius/models/Date.py
+++ ortelius/models/Date.py
@@ -1,4 +1,4 @@
-import math
+import sqlalchemy
 from ortelius import db
 
 
@@ -6,66 +6,74 @@ class Date(db.Model):
     """Date model"""
     __tablename__ = 'date'
 
-    def __init__(self, date=None, year=None, year_number=None):
+    def __init__(self, date=None, year=None):
         self.date = date
-        # self.year = year
-        self.year_number = year_number
+        self.year = year
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
+    date = db.Column(db.Date, nullable=False, unique=True)
     year_number = db.Column(db.Integer, db.ForeignKey('year.number'), nullable=True)
-    # year is declared in Year class via backref
+    year = db.relationship('Year', backref=db.backref('dates', uselist=True, lazy='dynamic'))
+
+    @classmethod
+    def create(cls, date=None):
+        """Create or get date if it already exist. Date should be datetime.date object"""
+        if not date:
+            raise Exception('Fields required: date')
+
+        year = Year.query.get(date.year)
+        new_date = cls(date=date, year=year)
+        db.session.add(new_date)
+        try:
+            db.session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            db.session.rollback()
+        finally:
+            return cls.query.filter_by(date=date).first()
+
     # hist_regions is declared in HistRegion class via backref
 
 
 class Year(db.Model):
-    """Year model"""
+    """
+        Year model. Contents year number as integer (e.g -879, 1678) and list of dates in this year.
+        Note that year does not method have 'create' and should be created beforehand, not dynamically.
+    """
     __tablename__ = 'year'
 
-    def __init__(self, number=None, dates=None, century=None, century_number=None):
+    def __init__(self, number=None, century=None):
         self.number = number
-        # self.century = century
-        self.century_number = century_number
-        if dates:
-            self.dates = dates
+        self.century = century
 
     number = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    dates = db.relationship('Date', backref=db.backref('year', lazy='select'), lazy='dynamic')
     century_number = db.Column(db.Integer, db.ForeignKey('century.number'), nullable=True)
-    # century is declared in Century class via backref
-
-    # def calculate_century(self):
-    #     return math.floor(self.number / 100) if (self.number / 100) < 0 else math.floor(self.number / 100) + 1
+    century = db.relationship('Century', backref=db.backref('years', uselist=True, lazy='dynamic'))
 
 
 class Century(db.Model):
-    """Century model"""
+    """
+        Century model. Contents century number as integer (e.g -10, 19) and list of years in this century.
+        Note that century does not method have 'create' and should be created beforehand, not dynamically.
+    """
     __tablename__ = 'century'
 
-    def __init__(self, number=None, years=None, millenium=None, millenium_number=None):
+    def __init__(self, number=None, millenium=None):
         self.number = number
-        # self.millenium = millenium
-        self.millenium_number = millenium_number
-        if years:
-            self.years = years
+        self.millenium = millenium
 
     number = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    years = db.relationship('Year', backref=db.backref('century'), lazy='dynamic')
     millenium_number = db.Column(db.Integer, db.ForeignKey('millenium.number'), nullable=True)
-    # millenium is declared in Millenium class via backref
-
-    # def calculate_millenium(self):
-    #     return math.floor(self.number / 10) if (self.number / 10) < 0 else math.floor(self.number / 10) + 1
+    millenium = db.relationship('Millenium', backref=db.backref('centuries', uselist=True, lazy='dynamic'))
 
 
 class Millenium(db.Model):
-    """Millenium model"""
+    """
+        Millenium model. Contents millenimum number as integer (e.g -2, 1) and list of centuries in this millenimum.
+        Note that millenium does not have method 'create' and should be created beforehand, not dynamically.
+    """
     __tablename__ = 'millenium'
 
-    def __init__(self, number=None, centuries=None):
+    def __init__(self, number=None):
         self.number = number
-        if centuries:
-            self.centuries = centuries
 
     number = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    centuries = db.relationship('Century', backref=db.backref('millenium'), lazy='dynamic')
