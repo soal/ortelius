1b1e70affb7e22d4bf6467cf88ca842a6bb8e1e4
soal <sorrow.about.alice@gmail.com>
2016-05-04 16:23:40 +0300

    Wikitext parsing middleware

--- ortelius/middleware.py
+++ ortelius/middleware.py
@@ -3,3 +3,15 @@ def serialize(sqlalchemy_obj):
     for field in sqlalchemy_obj._sa_class_manager.mapper.mapped_table.columns.keys():
         serialized[field] = sqlalchemy_obj.__getattribute__(field)
     return serialized
+
+def convert_wikitext(wikitext):
+    from smc import mw
+    from lxml import etree
+
+    ast = mw.MediaWiki(wikitext).ast
+    div = etree.Element('div')
+    elements = ast.getchildren()[0].getchildren()[0]
+    for elem in elements:
+        div.append(elem)
+
+    return etree.tounicode(div)
