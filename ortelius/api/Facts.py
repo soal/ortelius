51410a89adc8fb5d163523ca62d1f5281470d3fc
soal <sorrow.about.alice@gmail.com>
2016-05-12 04:14:38 +0300

    CLeanup, fix docs

--- ortelius/api/Facts.py
+++ ortelius/api/Facts.py
@@ -10,8 +10,8 @@ from ortelius.middleware import serialize, make_api_response
 
 '''
     Get facts with optional search params.
-    ?from=12-22-1560 - search facts from this date
-    ?to=03-30-1570 - search facts to this date
+    ?start_date=12-22-1560 - search facts from this date
+    ?end_date=03-30-1570 - search facts to this date
 
     ?topleft - coordinates of top left corner of the screen
     ?bottomright - coordinates bottom right corner of the screen
@@ -22,11 +22,10 @@ from ortelius.middleware import serialize, make_api_response
 
     Ex.:
     http://handymap.com/api/facts/36 - one fact by id
-    http://handymap.com/api/facts?from=12-22-1560&to=03-30-1570&topleft=65.45,56.89&bottomright=69.45,50.89 - facts by dates in given quadrant
+    http://handymap.com/api/facts?start_date=12-22-1560&end_date=03-30-1570&topleft=65.45,56.89&bottomright=69.45,50.89 - facts by dates in given quadrant
 '''
 
 def filter_by_time(query, start_date, end_date):
-    print('BY TIME', start_date, end_date)
     if start_date:
         start = hd(start_date)
     else:
@@ -43,23 +42,15 @@ def filter_by_time(query, start_date, end_date):
 
 def filter_by_geo(query, topleft, bottomright):
     if topleft and bottomright:
-        top_left = [float(x) for x in topleft.split(',')]
-        bottom_right = [float(x) for x in bottomright.split(',')]
+        top_left = [float(x) for x in topleft]
+        bottom_right = [float(x) for x in bottomright]
     else:
         return query
-    # CYCLE_START = time.clock()
     quadrants_coordinates = []
     for c in Quadrant.quadrants:
         if c[0] >= top_left[0] - 4 and c[0] <= bottom_right[0] and c[1] >= top_left[1]-4 and c[1] <= bottom_right[1]:
             quadrants_coordinates.append(','.join([str(c[0]), str(c[1])]))
 
-    # CYCLE_END = time.clock()
-    # print('CYCLE: ', CYCLE_END - CYCLE_START)
-    # GENERATOR_START = time.clock()
-    # quadrants_coordinates = [y for y in filter(lambda x: x[0] >= top_left[0]-4 and x[0] <= bottom_right[0], Quadrant.quadrants)]
-    # quadrants_coordinates = [ ','.join([str(z) for z in y]) for y in filter(lambda x: x[1] >= top_left[1]-4 and x[1] <= bottom_right[1], quadrants_coordinates)]
-    # GENERATOR_END = time.clock()
-    # print('GENERATOR: ', GENERATOR_END - GENERATOR_START)
     query = query.filter(Fact.shape.has(Shape.coordinates.any(Coordinates.quadrant_hash.in_(quadrants_coordinates))))
     return query
 
@@ -90,13 +81,12 @@ def filter_by_ids(query, ids):
         )
 def facts(start_date:hug.types.text=None,
           end_date:hug.types.text=None,
-          topleft:hug.types.text=None,
-          bottomright:hug.types.text=None,
+          topleft:list=None,
+          bottomright:list=None,
           weight:int=None,
           ids:list=None
           ):
     query = db.query(Fact)
-
     try:
         query = filter_by_time(query, start_date, end_date)
     except DateError as e:
@@ -108,7 +98,6 @@ def facts(start_date:hug.types.text=None,
     query = filter_by_weight(query, weight)
     query = filter_by_ids(query, ids)
     result = query.all()
-    print(result)
 
     serialized_result = []
     for fact in result:
@@ -125,7 +114,6 @@ def facts(start_date:hug.types.text=None,
         serialized.pop('text')
         serialized_result.append(serialized)
 
-    print(serialized_result)
     return make_api_response(serialized_result)
 
 @hug.get('/facts/{id}')
