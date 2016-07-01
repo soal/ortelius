fd9d466af0000c7ddeb14a3622b62b7de69c5b50
soal <sorrow.about.alice@gmail.com>
2016-07-01 21:20:20 +0300

    Update dicts response

--- ortelius/api/Dicts.py
+++ ortelius/api/Dicts.py
@@ -8,6 +8,6 @@ from ortelius.models.Element import ElementType
 @hug.get('/')
 def dicts():
     element_types = db.query(ElementType).all()
-    result = {}
-    result['element_types'] = [serialize(element_type) for element_type in element_types]
+    result = {'data': {}}
+    result['data']['element_types'] = [serialize(element_type) for element_type in element_types]
     return result
