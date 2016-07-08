c1f9ef9d695b24cdc806ba6e91fc3bd4b9aa6f71
soal <sorrow.about.alice@gmail.com>
2016-07-08 15:52:51 +0300

    Migration tools

---
+++ migrations/versions/870bef2e184a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 870bef2e184a
+Revises: ecee20f19b39
+Create Date: 2016-07-08 15:31:23.286584
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '870bef2e184a'
+down_revision = 'ecee20f19b39'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
